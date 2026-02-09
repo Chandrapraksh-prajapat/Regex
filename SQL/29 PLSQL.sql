c867165b8bdaccdc4d3d2b16e8b65964319da9eb
Chandraprakash prajapat <prajapatcp4747@gmail.com>
2026-02-09 10:50:29 +0530

    Start PL/SQL

---
+++ SQL/29 PLSQL.sql
@@ -0,0 +1,11 @@
+
+-- PL/SQL : Procedural Language where we combine the capability of SQL and programming language together 
+
+-- PL/SQL → adds logic on top of SQL (IF statements, loops, functions, procedures
+
+-- It lets you combine standard SQL (for querying data) with programming features like variables, loops,
+--  conditions, and error handling—so you can write powerful database-side programs.
+
+-- use for : Reusability
+
+-- allow use of all the control flow structure like if-else , while , for loop , functions etc....
